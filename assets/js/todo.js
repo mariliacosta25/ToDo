113ffea9a733d9fb32dcc90d0e17c86668088c13
Marilia Gabriela Costa Ferreira <m.costa.ferreira@avanade.com>
2022-04-29 15:10:06 -0300

    Aula5

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,4 +1,18 @@
+import { addTask,getAllTasks } from "./utils/domFunctions.js";
+
+
+
 // Banco de dados NoSQL - Orientado a Documentos. MongoDB, Firebase
+// Tarefas. Armazenar as tarefas em algum lugar. Por enquanto estamos armazenando na memória.
+// usamos uma constante. Armazena em memória é volátil.
+
+// Em localStorage e sessionStorage.
+
+// NodeJS(aprender semana que vem) - MongoDB ou Firebase. Armazenamento em Banco de dados. Armazenamento Persistente.
+// console.log(db[0].steps[2].step)
+//objeto é um conjunto de atributos(ou propriedades) e métodos(ou funções ou ações)
+// items: [{item:1}, {item:2},{item:3}]
+
 
 const db = [
   {
@@ -23,23 +37,48 @@ const db = [
     dueDate:"2022-04-28",
  },
 ];
-console.log(db[0].title);
 
-const newTask = document.querySelector("#inputTxtNewTask");
+getAllTasks(db);
+
+
+
+//console.log(db[0].title);
+// DOM - Document Object Model - é o JavaScript acessando o HTML e manipulando ele.
+// HTML ele é copilado pelo navegador em uma árvore de comandos, chamado DOM.
+
 const form = document.querySelector("#addNewTask");
+const newTask = document.querySelector("#inputTxtNewTask");
+
 form.addEventListener("submit",(e) => {
+  //Form faz um favor ? Pois não! Não faz nada...
     e.preventDefault();
 });
 newTask.addEventListener("keyup", (e) => {
+
     e.preventDefault();
     e.stopPropagation();
     if(e.key == "Enter") {
+      // Se newTask.value for ao contrário de true. Ou seja, se for false, se estiver vazio.  
+      if (!newTask.value) {
+        alert("Digite uma nova tarefa, para adicionar.");
+      } else {
     alert(newTask.value);
 
-    db.push({id: Number(db.length) + 1, title: newTask.value});
+    const d = new Date();
+    const today = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
+
+    db.push({
+      id: Number(db.length) + 1, 
+      title: newTask.value, 
+      done:false, 
+      dueDate: today,
+    });
+
+
     newTask.value = "";
     console.log(db);
  }
+}
 });
 
    
--- assets/js/utils/domFunctions.js
+++ assets/js/utils/domFunctions.js
@@ -4,14 +4,25 @@
 
 // S - SRO - Single Responsibility Principle
 // Princípio de responsabilidade Única
-
 // Funções bem inscritas devem, ser mantidas simples, curtas e realizando um objetivo.
-
 // quando precisamos acessar uma função em outro aqrquivo, é necessário eportá-la.
-
 // CRUD - Create Read Update Delete
-export function addTask() {
-  alert("Cadastrando uma nova tarefa");
+export function addTask(db, title="") {
+//  criando uma nova tarefa
+    const task = document.createElement('div');
+    // const id = Number(db.length) + 1;
+    task.classList.add('task');
+    task.setAttribute ('id',db.id);
+
+    const taskBody = `
+    <div> <input type="checkbox" id="check_${db.id}" /></div>
+                <div>
+                    <div><span class="title-task">${title?title:db.title}</span></div>
+            </div>
+            <div>Star</div>
+    `;
+    task.innerHTML = taskBody;
+    document.querySelector(".tasks") .appendChild(task);
 }
 export function removeTask() {
   alert("Removendo uma tarefa");
@@ -19,8 +30,11 @@ export function removeTask() {
 export function updateTask() {
   alert("Alterando uma nova tarefa");
 }
-export function getAllTask() {
-  alert("Listando todas tarefas");
+// colocamos chocolate apenas para explicar que pode ser qualquer nome.
+export function getAllTasks(chocolate) {
+  chocolate.forEach((item) => {
+    addTask (item);
+  });
 }
 export function getTaskById() {
   alert("Exibindo uma tarefa");
